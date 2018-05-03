8f8de4d53afff3878ca2e2646efffcfdc97d5cb0
Pavitar Dua <pavitardua@Mobiuso-iMac3.local>
2018-05-03 09:58:42 +0530

    Added project page

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ app.get('/bad',(req,res)=>{
         errorMessage:'Error in fulfilling this request'
     });
 });
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page'
+    });
+});
 app.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 });
